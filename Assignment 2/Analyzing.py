a42b20481a2dbf286ddb8cb7eaa7cda5de7cffa5
angelawxr <60156572+angelawxr@users.noreply.github.com>
2020-03-22 21:47:21 -0400

    assignment 2

--- Assignment 2/Analyzing.py
+++ Assignment 2/Analyzing.py
@@ -1,33 +1,10 @@
-#Characterizing by Word Frequencies
-
-dict = dict()
-for line in fp:
-    line = line.rstrip()
-    words = line.split()
-    for w in words:
-       dict[w] = dict.get(w,0)+1 
-
-print(dict)
-
-#Computing Summary Statistics
-largest = -10
-for wd, freq in dict.items():
-    print(wd, freq)
-    if value > largest:
-        largest = value 
-        theword = k 
-print(theword, largest)
-
-
 import random
 import string
 
-
+#Data Cleaning
 def process_file(filename, skip_header):
-    """Makes a histogram that contains the words from a file.
-    filename: string
-    skip_header: boolean, whether to skip the Gutenberg header
-    returns: map from each word to the number of times it appears.
+    """
+    Makes a histogram that contains the words from a file.
     """
     hist = {}
     fp = open(filename, encoding='UTF8')
@@ -44,84 +21,62 @@ def process_file(filename, skip_header):
         line = line.replace('-', ' ')
 
         for word in line.split():
-            # word could be 'Sussex.'
             word = word.strip(strippables)
             word = word.lower()
 
-            # update the dictionary
             hist[word] = hist.get(word, 0) + 1
 
     return hist
 
 
 def skip_gutenberg_header(fp):
-    """Reads from fp until it finds the line that ends the header.
-    fp: open file object
+    """
+    Reads from fp until it finds the line that ends the header.
     """
     for line in fp:
         if line.startswith('*** START OF THIS PROJECT'):
             break
 
-
-def total_words(hist):
-    """Returns the total of the frequencies in a histogram."""
-    return sum(hist.values())
-
-
-def different_words(hist):
-    """Returns the number of different words in a histogram."""
-    return len(hist) 
-
-
+#Characterizing by Word Frequencies
 def most_common(hist, excluding_stopwords = False):
-    """Makes a list of word-freq pairs in descending order of frequency.
+    """
+    Makes a list of word-freq pairs in descending order of frequency.
     hist: map from word to frequency
     returns: list of (frequency, word) pairs
     """
     t = [] 
 
-    stopwords = process_file('session13/stopwords.txt', skip_header=False)
+    stopwords = process_file('Assignment 2/stopwords.txt', skip_header=False)
 
     stopwords = list(stopwords.keys()) 
-
     for word, freq in hist.items():
         if excluding_stopwords:
             if word in stopwords:
                 continue
-
         t.append((freq, word)) 
     t.sort(reverse=True)
     return t
 
-
-def print_most_common(hist, num=10):
-    """Prints the most commons words in a histgram and their frequencies.
+#Computing Summary Statistics
+#Top 10 words 
+def print_most_common(hist, num=10, excluding_stopwords = False):
+    """
+    Prints the most commons words in a histgram and their frequencies.
     hist: histogram (map from word to frequency)
     num: number of words to print
     """
-    pass
+    t = most_common(hist,excluding_stopwords = True)
+    print('The most common words are:')
+    for freq, word in t[:num]:
+        print(word, freq, sep = '\t')
+
+
 
 def main():
     hist = process_file('Assignment 2/Walden.txt', skip_header=True)
     print(hist)
-    print('Total number of words:', total_words(hist))
-    print('Number of different words:', different_words(hist))
-
-    t = most_common(hist)
-    print('The most common words are:')
-    for freq, word in t[0:20]:
-        print(word, '\t', freq)
-
-    # words = process_file('words.txt', skip_header=False)
-
-    # diff = subtract(hist, words)
-    # print("The words in the book that aren't in the word list are:")
-    # for word in diff.keys():
-    #     print(word, end=' ')
-
-    # print("\n\nHere are some random words from the book")
-    # for i in range(100):
-    #     print(random_word(hist), end=' ')
+    most_common(hist, excluding_stopwords = False)
+    print_most_common(hist,10,excluding_stopwords = False)
 
 
 if __name__ == '__main__':
