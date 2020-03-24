05f96c4af65f2bc3cf33ca7b42a4232dde155854
DanJamRod <rodgersd09@gmail.com>
2020-03-23 21:55:51 -0400

    Update wip.py

--- wip.py
+++ wip.py
@@ -3,10 +3,10 @@
 import tweepy
 
 # Codes availiable in final documentation
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+access_token = "173816701-5oYZgixJxTAO0dyE1EyYQivHFJqUDi9CqpsNFxXt"
+access_token_secret =
+consumer_key = "U0S0rabX7Kwhw5mN8phJP5uRa"
+consumer_secret = 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -14,6 +14,8 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 alltweets = api.user_timeline(screen_name = "realDonaldTrump", count=200)
-tweets = [[tweet.text] for tweet in alltweets]
+tweets = [[tweet.text.lower()] for tweet in alltweets]
 
-print(tweets)
+
+for tweet in alltweets:
+    print(tweets)
