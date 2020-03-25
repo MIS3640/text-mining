9f5d5314a680f1728a426bf032e17bac38bf889c
angelawxr <60156572+angelawxr@users.noreply.github.com>
2020-03-25 15:21:03 -0400

    Update text_clustering.py

--- Assignment 2/text_clustering.py
+++ Assignment 2/text_clustering.py
@@ -1,25 +1,42 @@
+import pandas as pd
 import numpy as np
-from sklearn.manifold import MDS
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-# these are the similarities computed from the previous section
-S = np.asarray([[1., 0.90850572, 0.96451312, 0.97905034, 0.78340575],
-    [0.90850572, 1., 0.95769915, 0.95030073, 0.87322494],
-    [0.96451312, 0.95769915, 1., 0.98230284, 0.83381607],
-    [0.97905034, 0.95030073, 0.98230284, 1., 0.82953109],
-    [0.78340575, 0.87322494, 0.83381607, 0.82953109, 1.]])
+from sklearn.datasets import load_files
 
-# dissimilarity is 1 minus similarity
-dissimilarities = 1 - S
+import os
+print(os.getcwd())
 
-# compute the embedding
-coord = MDS(dissimilarity='precomputed').fit_transform(dissimilarities)
+# for reproducibility
+data = load_files('Assignment 2/Walden.txt',description=None,categories=None,load_content=True,shuffle=True,encoding="utf8", decode_error='replace', random_state=0)
+# df = pd.DataFrame(list(zip(data['data'], data['target'])), columns=['text', 'label'])
+# df.head()
 
-plt.scatter(coord[:, 0], coord[:, 1])
+# vec = TfidfVectorizer(stop_words="english")
+# vec.fit(df.text.values)
+# features = vec.transform(df.text.values)
 
-# Label the points
-for i in range(coord.shape[0]):
-    plt.annotate(str(i), (coord[i, :]))
+# cls = MiniBatchKMeans(n_clusters=5, random_state=random_state)
+# cls.fit(features)
+
+# # predict cluster labels for new dataset
+# cls.predict(features)
+
+# # to get cluster labels for the dataset used while
+# # training the model (used for models that does not
+# # support prediction on new dataset).
+# cls.labels_
 
 
-plt.show()
+# # reduce the features to 2D
+# pca = PCA(n_components=2, random_state=random_state)
+# reduced_features = pca.fit_transform(features.toarray())
+
+# # reduce the cluster centers to 2D
+# reduced_cluster_centers = pca.transform(cls.cluster_centers_)
+
+# plt.scatter(reduced_features[:,0], reduced_features[:,1], c=cls.predict(features))
+# plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:,1], marker='x', s=150, c='b')
