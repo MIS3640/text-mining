72385ac82dfd579a83953acd37fd325e02baaaa7
angelawxr <60156572+angelawxr@users.noreply.github.com>
2020-03-25 15:21:14 -0400

    Update text_clustering.py

--- Assignment 2/text_clustering.py
+++ Assignment 2/text_clustering.py
@@ -14,29 +14,3 @@ print(os.getcwd())
 data = load_files('Assignment 2/Walden.txt',description=None,categories=None,load_content=True,shuffle=True,encoding="utf8", decode_error='replace', random_state=0)
 # df = pd.DataFrame(list(zip(data['data'], data['target'])), columns=['text', 'label'])
 # df.head()
-
-# vec = TfidfVectorizer(stop_words="english")
-# vec.fit(df.text.values)
-# features = vec.transform(df.text.values)
-
-# cls = MiniBatchKMeans(n_clusters=5, random_state=random_state)
-# cls.fit(features)
-
-# # predict cluster labels for new dataset
-# cls.predict(features)
-
-# # to get cluster labels for the dataset used while
-# # training the model (used for models that does not
-# # support prediction on new dataset).
-# cls.labels_
-
-
-# # reduce the features to 2D
-# pca = PCA(n_components=2, random_state=random_state)
-# reduced_features = pca.fit_transform(features.toarray())
-
-# # reduce the cluster centers to 2D
-# reduced_cluster_centers = pca.transform(cls.cluster_centers_)
-
-# plt.scatter(reduced_features[:,0], reduced_features[:,1], c=cls.predict(features))
-# plt.scatter(reduced_cluster_centers[:, 0], reduced_cluster_centers[:,1], marker='x', s=150, c='b')
