18976b318cb396520b4e794d5c0a5912042ae144
zoeechengg <zcheng3@babson.edu>
2020-03-27 09:50:48 -0400

    Update analyze_data.py

--- analyze_data.py
+++ analyze_data.py
@@ -1,4 +1,6 @@
 import string
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 def process_file(filename, skip_header):
     """Makes a histogram that contains the words from a file.
@@ -9,7 +11,7 @@ def process_file(filename, skip_header):
     returns: map from each word to the number of times it appears.
     """
     hist = {}
-    fp = open(filename)
+    fp = open(filename, encoding='UTF8')
 
     # if skip_header:
     #     skip_gutenberg_header(fp)
@@ -17,13 +19,9 @@ def process_file(filename, skip_header):
     strippables = string.punctuation + string.whitespace
 
     for line in fp:
-        # if line.startswith('*** END OF THIS PROJECT'):
-        #     break
-        
         line = line.replace('-', ' ')
 
         for word in line.split():
-            # word could be 'Sussex.'
             word = word.strip(strippables)
             word = word.lower()
 
@@ -31,16 +29,6 @@ def process_file(filename, skip_header):
             hist[word] = hist.get(word, 0) + 1
 
     return hist
-
-
-# def skip_gutenberg_header(fp):
-#     """Reads from fp until it finds the line that ends the header.
-
-#     fp: open file object
-#     """
-#     for line in fp:
-#         if line.startswith('*** START OF THIS PROJECT'):
-#             break
 
 
 def total_words(hist):
@@ -76,52 +64,23 @@ def most_common(hist, excluding_stopwords = False):
     return t
 
 
-def print_most_common(hist, num=10):
-    """Prints the most commons words in a histgram and their frequencies.
-
-    hist: histogram (map from word to frequency)
-    num: number of words to print
-    """
-    pass
-
-
-def subtract(d1, d2):
-    """Returns a dictionary with all keys that appear in d1 but not d2.
-
-    d1, d2: dictionaries
-    """
-    pass
-
-
-def random_word(hist):
-    """Chooses a random word from a histogram.
-
-    The probability of each word is proportional to its frequency.
-    """
-    pass
+def sentiment_analysis(filename):
+    score = SentimentIntensityAnalyzer().polarity_scores(filename)
+    print(score)
 
 
 def main():
-    hist = process_file('tweets.txt', skip_header=False)
-    # print(hist)
+    hist = process_file('tweets.txt', skip_header = False)
+    print(hist)
     print('Total number of words:', total_words(hist))
     print('Number of different words:', different_words(hist))
 
-    t = most_common(hist, excluding_stopwords=True)
+    t = most_common(hist, excluding_stopwords = True)
     print('The most common words are:')
-    for freq, word in t[0:20]:
+    for freq, word in t[0:40]:
         print(word, '\t', freq)
 
-    # words = process_file('words.txt', skip_header=False)
-
-    # diff = subtract(hist, words)
-    # print("The words in the book that aren't in the word list are:")
-    # for word in diff.keys():
-    #     print(word, end=' ')
-
-    # print("\n\nHere are some random words from the book")
-    # for i in range(100):
-    #     print(random_word(hist), end=' ')
+    print(sentiment_analysis('tweets.txt'))
 
 
 if __name__ == '__main__':
