4c46e3bdeb4e37a6f09d975208043c870298b652
zoeechengg <zcheng3@babson.edu>
2020-03-27 09:50:54 -0400

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -8,7 +8,7 @@ with open('tweets.p', 'rb') as p:
 tweet_text = [] 
 for tweet in t['statuses']:
     tweet_text.append(tweet['text'])
-print(tweet_text)
+# print(tweet_text)
 
 def convert_to_single_words(tweet_text):
     """
@@ -16,13 +16,14 @@ def convert_to_single_words(tweet_text):
     """
     return ' '.join(tweet_text).split()
 new_tweet_text = (convert_to_single_words(tweet_text))
-# print(new_tweet_text)
+print(new_tweet_text)
 
 # Writing the new_tweet_text list into a text file 
-new_file = open('tweets.txt', 'w')
+new_file = open('tweets.txt', 'w', encoding='utf-8')
 for element in new_tweet_text:
     new_file.write(element)
     new_file.write('\n')
+# new_file.writelines(new_tweet_text, '\n')
 new_file.close()
 
 # hist = {}
