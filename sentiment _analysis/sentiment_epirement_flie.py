e988ee4b218b4b27b07b30b4cd3cc37c233d9f80
E-WOLFSON <E-WOLFSON@users.noreply.github.com>
2020-03-28 16:09:19 -0400

    updated.py

--- sentiment _analysis/sentiment_epirement_flie.py
+++ sentiment _analysis/sentiment_epirement_flie.py
@@ -1,5 +1,7 @@
 '''
 this is an expiriemental file, it generates a twitter stream based on keywords
+this is not apart of my main project, just wanted to keep it in the folder for reference later 
+when I go to build this out furture for my own purposes. 
 '''
 
 
