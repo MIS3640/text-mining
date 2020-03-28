33d5fce334ed9e8ed2e8270b97b85f1b4efd4551
ikim1 <60156429+ikim1@users.noreply.github.com>
2020-03-27 21:20:43 -0700

    Update text_mining.py

--- text_mining.py
+++ text_mining.py
@@ -2,20 +2,20 @@ from imdbpie import Imdb
 import random
 
 imdb = Imdb()
-print(imdb.search_for_title("Parasite")[0])
+parasite = (imdb.search_for_title("Parasite")[0])
 reviews = imdb.get_title_user_reviews("tt6751668")
 
-print(imdb.search_for_title("Joker")[0])
+joker = (imdb.search_for_title("Joker")[0])
 review2 = imdb.get_title_user_reviews("tt7286456")
 
 
-print(imdb.search_for_title("Unplanned")[0])
+unplanned = (imdb.search_for_title("Unplanned")[0])
 review3 = imdb.get_title_user_reviews("tt9024106")
 
-print(imdb.search_for_title("The Godfather")[0])
+thegodfather = (imdb.search_for_title("The Godfather")[0])
 reviews3 = imdb.get_title_user_reviews("tt0068646")
 
-print(imdb.search_for_title("Disaster Movie")[0])
+disastermovie = (imdb.search_for_title("Disaster Movie")[0])
 reviews2 = imdb.get_title_user_reviews("tt1213644")
 
 
@@ -35,29 +35,29 @@ reviews2 = imdb.get_title_user_reviews("tt1213644")
 def printreviews(reviews):
     p = []
     for n in reviews['reviews']:
-        p.append(random.choice(n['reviewText']))
+        p.append(n['reviewText'])
     return p
 
 
 parasitereviews = printreviews(reviews)
 parasitereviews = ",".join(parasitereviews)
-print(parasitereviews)
+# print(parasitereviews)
 
 jokerreviews = printreviews(review2)
 jokerreviews = ",".join(jokerreviews)
-print(jokerreviews)
+# print(jokerreviews)
 
 unplannedreviews = printreviews(review3)
 unplannedreviews = ",".join(unplannedreviews)
-print(unplannedreviews)
+# print(unplannedreviews)
 
 godfather = printreviews(reviews3)
 godfather = ",".join(godfather)
-print(godfather)
+# print(godfather)
 
 DisasterMovie = printreviews(reviews2)
 DisasterMovie = ",".join(DisasterMovie)
-print(DisasterMovie)
+# print(DisasterMovie)
 
 import nltk
 nltk.download('vader_lexicon')
@@ -65,16 +65,78 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
 score = SentimentIntensityAnalyzer().polarity_scores(parasitereviews)
-print(score)
+# print(score)
 
 score2 = SentimentIntensityAnalyzer().polarity_scores(jokerreviews)
-print(score2)
+# print(score2)
 
 score3 = SentimentIntensityAnalyzer().polarity_scores(unplannedreviews)
-print(score3)
+# print(score3)
 
 score5 = SentimentIntensityAnalyzer().polarity_scores(godfather)
-print(score5)
+# print(score5)
 
 score4 = SentimentIntensityAnalyzer().polarity_scores(DisasterMovie)
-print(score4)
+# print(score4)
+
+
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print(f"Movie Title: {parasite['title']} | Year: {parasite['year']} ")
+print()
+print(f"The Positive Valence for this movie is {score['pos']}")
+print(f"The Negative Valence for this movie is {score['neg']}")
+print(f"The Neutral Valence for this movie is {score['neu']}")
+print(f"The Compoud for this movie is {score['compound']}")
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print(f"Movie Title: {joker['title']} | Year: {joker['year']} ")
+print()
+print(f"The Positive Valence for this movie is {score2['pos']}")
+print(f"The Negative Valence for this movie is {score2['neg']}")
+print(f"The Neutral Valence for this movie is {score2['neu']}")
+print(f"The Compoud for this movie is {score2['compound']}")
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print(f"Movie Title: {unplanned['title']} | Year: {unplanned['year']} ")
+print()
+print(f"The Positive Valence for this movie is {score3['pos']}")
+print(f"The Negative Valence for this movie is {score3['neg']}")
+print(f"The Neutral Valence for this movie is {score3['neu']}")
+print(f"The Compoud for this movie is {score3['compound']}")
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print(f"Movie Title: {thegodfather['title']} | Year: {thegodfather['year']} ")
+print()
+print(f"The Positive Valence for this movie is {score5['pos']}")
+print(f"The Negative Valence for this movie is {score5['neg']}")
+print(f"The Neutral Valence for this movie is {score5['neu']}")
+print(f"The Compoud for this movie is {score5['compound']}")
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+print()
+print(f"Movie Title: {disastermovie['title']} | Year: {disastermovie['year']} ")
+print()
+print(f"The Positive Valence for this movie is {score4['pos']}")
+print(f"The Negative Valence for this movie is {score4['neg']}")
+print(f"The Neutral Valence for this movie is {score4['neu']}")
+print(f"The Compoud for this movie is {score4['compound']}")
+print()
+print("---------------------------------------------------------------------------------------------------------------------------")
+
+
