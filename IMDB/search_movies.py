c68054dbc934f79c38bbab619270b69f184baa78
camwolfe <cwolfe2@babson.edu>
2020-10-18 13:32:29 -0700

    Update search_movies.py

--- IMDB/search_movies.py
+++ IMDB/search_movies.py
@@ -5,9 +5,9 @@ moviesDB = imdb.IMDb()
 
 movies = moviesDB
 
-movies = moviesDB.search_movie("The Shawshank Redemption")
+movies = moviesDB.search_movie("Borat")
 
-# 1) Searching for a title of a movie (returns all under title)
+# 1) Searching for a title of a movie (returns all movies under title given)
 """ `
 for movie in movies:
     title = movie["title"]
@@ -17,6 +17,7 @@ for movie in movies:
 
 # 2) Listing movie info
 
+# below variables are used for #3 as well
 id = movies[0].getID()
 movie = moviesDB.get_movie(id)
 
@@ -26,19 +27,24 @@ rating = movie["rating"]
 directors = movie["directors"]
 casting = movie["cast"]
 
-print("movie info")
+
+print("MOVIE INFO")
+# Prints title and year of movie
 print(f"{title} - {year}")
+# IMDB rating
 print(f"rating: {rating}")
 
+# Pulls direcotr of film
 direcStr = " ".join(map(str, directors))
 print(f"directors: {direcStr}")
 
+# pulls actors in film
 actors = ", ".join(map(str, casting))
 print(f"actors: {actors}")
 
 
-# 3) Actor info - needs debugging
-'''
+# 3) Actor info
+# Pulls info on the primary actor listed
 id = casting[0].getID()
 person = moviesDB.get_person(id)
 bio = moviesDB.get_person_biography(id)
@@ -49,11 +55,15 @@ height = person["height"]
 trivia = person["trivia"]
 titleRefs = bio["titlesRefs"]
 
+# actor name
 print(f"name: {name}")
+# actor birthday
 print(f"birth date: {birthDate}")
+# actor height
 print(f"height: {height}")
+# actor trivia
 print(f"trivia: {trivia[0]}")
 
 titleRefsStr = ", ".join(map(str, titleRefs))
 print(f"bio title refs: {titleRefsStr}")
-'''
+
