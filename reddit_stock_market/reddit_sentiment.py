d70515e922a9df7df95c363f369ec8616eaac986
kevluu20 <54873215+kevluu20@users.noreply.github.com>
2020-10-18 23:11:59 -0400

    Update reddit_sentiment.py

--- reddit_stock_market/reddit_sentiment.py
+++ reddit_stock_market/reddit_sentiment.py
@@ -10,3 +10,31 @@ sns.set(style='darkgrid', context='talk', palette='Dark2')
 
 
 import praw
+
+reddit = praw.Reddit(
+    client_id="MtwTkf_DOPpB_A",
+    client_secret="hW_ZKOsPCtr1t2gpy_REhSCG1cE",
+    username="kluu2020",
+    password="Malden123!",
+    user_agent="Reddit Text Mining Version 1",
+)
+
+headlines = set()
+
+for submission in reddit.subreddit('politics').new(limit=100):
+    headlines.add(submission.title)
+    display.clear_output()
+    # print(len(headlines))
+
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+
+sia = SIA()
+results = []
+
+for line in headlines:
+    pol_score = sia.polarity_scores(line)
+    pol_score['headline'] = line
+    results.append(pol_score)
+
+pprint(results[:3], width=100)
