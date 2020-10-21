0c6b464c22c24d87d1e4e7be988b16901c05860e
camwolfe <cwolfe2@babson.edu>
2020-10-20 17:15:24 -0700

    Update IMDB_data.py

--- IMDB/IMDB_data.py
+++ IMDB/IMDB_data.py
@@ -82,9 +82,6 @@ print(imdb.search_for_title("The Shawshank Redemption")[0])
 # 2) imdb id
 reviews = imdb.get_title_user_reviews("tt0111161")
 
-# import pprint
-# pprint.pprint(reviews)
-
 print(reviews["reviews"][0]["author"]["displayName"])
 print(reviews["reviews"][0]["reviewText"])
 
