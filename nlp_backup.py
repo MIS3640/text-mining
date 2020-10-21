65e9e43bd5b7c8bee0b83fbb2256e540bba05e1a
nchapin1 <70173250+nchapin1@users.noreply.github.com>
2020-10-21 00:53:00 -0400

    pain

---
+++ nlp_backup.py
@@ -0,0 +1,118 @@
+# from nltk.corpus import brown
+# print(brown.words())
+
+from twython import Twython
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
+
+darth = SentimentIntensityAnalyzer()
+
+from config import TOKEN_SECRET_HIDDEN, CONSUMER_SECRET_HIDDEN
+import re, unicodedata
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %matplotlib inline
+
+
+""" 
+Resources:
+Twitter Search - https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
+Twitter Standard Operators - https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/guides/standard-operators
+Twython Search - https://twython.readthedocs.io/en/latest/usage/starting_out.html#dynamicfunctionarguments
+ """
+# TODO:
+# 1. Figure out how to hide the config.py (Unless if he needs it to run the code)
+# 2. Is this actually doing anything? "Truncated = False"
+# 3. Figure out if need to tokenize the tweets
+# 4. Docstrings
+# 5. What to improve in the code? - check instructions to ensure all points have been covered
+# 6. Maybe make the "tweet = ..." edits more efficient
+# 7. Potentially have words analysis
+# 8. Print the original tweet vs cleaned tweet for tweets_nlp?
+# 9. Test how accurate the analyzer is - use known sentences and known scores to see if implementation is correct
+
+def fetch_tweets():
+    TOKEN = "1308753867992170498-y2tPq70sOhS9VxNeRhnuO4wW6cIdXH"
+    TOKEN_SECRET = TOKEN_SECRET_HIDDEN
+    CONSUMER_KEY = "AmSWYiODOWhTIWLo7R09nh3sJ"
+    CONSUMER_SECRET = CONSUMER_SECRET_HIDDEN
+
+    t = Twython(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET)
+    # data - dict, data['statuses'] - list, status - dict, status['text'] - str
+    tlist = list()
+    data = t.search(
+        q="trump",
+        lang="en",
+        result_type="recent",
+        count=2,
+        tweet_mode="extended",
+        truncated=False,
+    )
+    for status in data["statuses"]:
+        tweet = (
+            status["full_text"].lower().replace("\n", "")
+        )  # remove whitespace and make it lowercase
+        tweet = "".join(re.sub(r"http\S+", "", tweet))  # remove URL
+        tweet = re.sub(r"[^\w\s]", "", tweet)  # remove punctuation
+        tweet = (
+            unicodedata.normalize("NFKD", tweet)
+            .encode("ascii", "ignore")
+            .decode("utf-8", "ignore")
+        )  # remove emojis
+        tlist.append(tweet)
+    return tlist
+
+
+def tweets_nlp(tweets):
+    """"""
+    scoredict = dict()
+    for tweet in tweets:
+        score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+        print(score)
+        # scoredict[tweet] = score
+        # sentence = word_tokenize(tweet)
+        # print(f"Tweet: {tweet}")
+        # print(f"Score: {score}")
+        # print()
+    # return df
+    # return scoredict
+    
+
+def pandascore(scores):
+    # df = pd.DataFrame(list(scores.items()), columns = ['Neg', 'Neu', 'Pos', 'Compound'])
+    # df = pd.DataFrame.from_dict(scores, orient='index')
+    df = pd.DataFrame.from_dict(scores)
+    df['label'].value_counts()
+    # HISTOGRAM
+    # fig, ax = plt.subplots(figsize=(8,6))
+    # df.hist(bins=[-1, -0.75, -0.5, -0.25, 0.25, 0.5, 0.75, 1],
+    # ax = ax,
+    # color = 'purple')
+    # plt.title("Sentiments from Tweets")
+    # plt.show()
+    
+    # tweettext = scores.keys()
+    # scoring = scores.values()
+    # plt.bar(tweettext, scoring)
+
+    # lists = sorted(scores.items())
+    # x, y = zip(*lists)
+
+    # plt.plot(x,y)
+    # plt.show()
+
+    # print(df)
+
+def main():
+    raw_tweets = fetch_tweets()
+    # scores = scoredict
+    # print(pandascore(tweets_nlp(tweets)))
+    returned_scoredict = tweets_nlp(raw_tweets)
+    print(tweets_nlp(raw_tweets))
+    # print(pandascore(returned_scoredict))
+
+if __name__ == "__main__":
+    main()
--- nlp_token_backup.py
+++ nlp_token_backup.py
@@ -1,20 +1,15 @@
 from twython import Twython
 from config import TOKEN_SECRET_HIDDEN, CONSUMER_SECRET_HIDDEN
-import re, unicodedata
+import string, re, unicodedata
+import nltk from nltk.corpus import stopwords set(stopwords.words('english'))
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+from nltk.corpus import twitter_samples
+positive = twitter_samples.strings('positive_tweets.json')
+negative = twitter_samples.strings('negative_tweets.json')
+text = twitter_samples.strings('tweets.20150430-223406.json')
+tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 
-# %matplotlib inline
-
-
-""" 
-Resources:
-Twitter Search - https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
-Twitter Standard Operators - https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/guides/standard-operators
-Twython Search - https://twython.readthedocs.io/en/latest/usage/starting_out.html#dynamicfunctionarguments
- """
+print(tweet_tokens)
 def fetch_tweets():
     TOKEN = "1308753867992170498-y2tPq70sOhS9VxNeRhnuO4wW6cIdXH"
     TOKEN_SECRET = TOKEN_SECRET_HIDDEN
@@ -23,16 +18,17 @@ def fetch_tweets():
 
     t = Twython(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET)
     # data - dict, data['statuses'] - list, status - dict, status['text'] - str
-    tlist = list()
+    count = {}  # Acts as a counter for how many times a word appears 
     data = t.search(
         q="trump",
         lang="en",
         result_type="recent",
-        count=2,
+        count= 3,
         tweet_mode="extended",
         truncated=False,
     )
     for status in data["statuses"]:
+        tweet = status["full_text"]
         tweet = (
             status["full_text"].lower().replace("\n", "")
         )  # remove whitespace and make it lowercase
@@ -43,10 +39,15 @@ def fetch_tweets():
             .encode("ascii", "ignore")
             .decode("utf-8", "ignore")
         )  # remove emojis
-        tlist.append(tweet)
-    return tlist
+        tweet = list(tweet.split(" "))    # make each tweet a list of words
+        for word in tweet:
+            count[word] = count.get(word,0) + 1 # word frequency count
+    return count
+
+def tokenize_count(count): 
 
 def main():
+    print(fetch_tweets())
 
 if __name__ == "__main__":
     main()
