0e0e288709ca0ecc586646ccb08dd655e24cb0b8
James Lee <jlee22@babson.edu>
2021-03-11 14:05:22 -0500

    Update text_mining.py

--- text_mining.py
+++ text_mining.py
@@ -2,47 +2,58 @@ import praw
 import pickle
 import csv
 
+
 def stock_list():
-    '''opens csv file and creates a list with all of the stock tickers in the US'''
-    with open('stock.csv', newline='') as csvfile:
+    """opens csv file and creates a list with all of the stock tickers in the US"""
+    with open("stock.csv", newline="") as csvfile:
         reader = csv.DictReader(csvfile)
         lst = []
         for row in reader:
-            lst.append(row['ticker'])
+            lst.append(row["ticker"])
     return lst
 
 
 def import_subreddit():
-    '''imports the top 1000 post titles under wallstreetbets subreddit'''
-    reddit = praw.Reddit(client_id = 'UV_0pi49dIB77w', client_secret = 'xrAIGSNfOco13rjEJii23FB7EDPJBQ', username = 'verkxies1', password = 'problemsolvingandsoftwaredesign', user_agent = 'verkxies1')
-    sub = 'wallstreetbets'
-    submissions = reddit.subreddit(sub).top('day', limit = 50)
+    """imports the top 1000 post titles under wallstreetbets subreddit"""
+    reddit = praw.Reddit(
+        client_id="UV_0pi49dIB77w",
+        client_secret="xrAIGSNfOco13rjEJii23FB7EDPJBQ",
+        username="verkxies1",
+        password="problemsolvingandsoftwaredesign",
+        user_agent="verkxies1",
+    )
+    sub = "wallstreetbets"
+    submissions = reddit.subreddit(sub).top("day", limit=50)
     top = [(submission.title) for submission in submissions]
     return top
 
+
 def word_parse():
-    '''a list of titles with a sublist that parses each word in the title into a list''' 
+    """a list of titles with a sublist that parses each word in the title into a list"""
     lst = []
     for title in import_subreddit():
         lst.append(title.split())
-    return(lst)
+    return lst
+
 
 def ticker_check(word):
-    '''This function checks if the word is a stock ticker'''
-    if word.replace('$', '') in stock_list():
+    """This function checks if the word is a stock ticker"""
+    if word.replace("$", "") in stock_list():
         return True
     return False
 
+
 def ticker_one(title):
-    '''This function creates a dictionary of all stock tickers that appear in a title, assigning a value of 1 to each'''
+    """This function creates a dictionary of all stock tickers that appear in a title, assigning a value of 1 to each"""
     d = {}
     for word in title:
         if ticker_check(word):
-            d[word.replace('$', '')] = 1
+            d[word.replace("$", "")] = 1
     return d
 
+
 def ticker_count():
-    '''Counts all the posts where a ticker is mentioned and stores it in a dictionary with the ticker as the key'''
+    """Counts all the posts where a ticker is mentioned and stores it in a dictionary with the ticker as the key"""
     d = {}
     for title in word_parse():
         for key in ticker_one(title):
@@ -51,7 +62,6 @@ def ticker_count():
             else:
                 d[key] = ticker_one(title)[key]
     return d
-        
 
 
 def main():
@@ -67,4 +77,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
