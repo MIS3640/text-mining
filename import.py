004d63d974c2cc1487680ac7f31e225e9d56c22c
Raphaeldunand <57508048+Raphaeldunand@users.noreply.github.com>
2021-03-12 16:32:05 -0500

    Update import.py

--- import.py
+++ import.py
@@ -1,7 +1,14 @@
 import urllib.request
 
-url = 'http://www.gutenberg.org/ebooks/1080.txt.utf-8'
-response = urllib.request.urlopen(url)
+#The Project Gutenberg eBook of A Modest Proposal, by Jonathan Swift
+url1 = 'http://www.gutenberg.org/files/1080/1080-0.txt'
+response1 = urllib.request.urlopen(url1)
+data = response.read()  # a `bytes` object
+text = data.decode('utf-8')
+print(text) # for testing
+
+url2 = 'http://www.gutenberg.org/files/1080/1080-0.txt'
+response1 = urllib.request.urlopen(url1)
 data = response.read()  # a `bytes` object
 text = data.decode('utf-8')
 print(text) # for testing
