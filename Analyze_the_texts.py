1e4963e08ca88fd7000a1d6a09db0d84583beb7a
Emmy Ma <hma@babson.edu>
2021-03-16 10:16:44 -0400

    Update Analyze_the_texts.py

--- Analyze_the_texts.py
+++ Analyze_the_texts.py
@@ -8,6 +8,10 @@ url1 = 'http://www.gutenberg.org/files/1080/1080-0.txt'
 response1 = urllib.request.urlopen(url1)
 data1 = response1.read()  # a `bytes` object
 text1 = data1.decode('utf-8')
+url2 = ''
+response2 = urllib.request.urlopen(url2)
+data2 = response2.read()  # a `bytes` object
+text2 = data2.decode('utf-8')
 print(text1) # for testing
 
 
