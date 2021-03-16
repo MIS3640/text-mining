2230a370602aec0474007e28b4bc924c6e744846
SammiXu0 <77679804+SammiXu0@users.noreply.github.com>
2021-03-16 21:19:04 +0800

    Updated version, can now save imdb to a local file.

--- example1.py
+++ example1.py
@@ -3,27 +3,62 @@ from imdbpie import Imdb
 import pprint
 
 imdb = Imdb()
-print(imdb.search_for_title('Star Wars: Episode IV - A New Hope')[0])
-reviews = imdb.get_title_user_reviews('tt0076759')['reviews']
-review_text = {}
-for r in reviews:
-    rt = r['reviewText']
-    auth = r['author']['displayName']
-    review_text[auth] = rt
 
-pprint.pprint(review_text)
+# Get movie imdb-id
 
-# print(reviews['reviews'][0]['author']['displayName'])
-# print(reviews['reviews'][0]['authorRating'])
-# print(reviews['reviews'][0]['reviewText'])
+movie_name = [
+    "Star Wars: Episode I - The Phantom Menace",
+    "Star Wars: Episode II - Attack of the Clones",
+    "Star Wars: Episode III - Revenge of the Sith",
+]
 
-# example_text = reviews['reviews'][0:3]
+# Get info from imdb (title, id, number of reviews, reviews text)
+def get_info(movie_list):
+    '''
+    Retrieves movie title, movie ID, number of reviews, rating of the review, 
+    and the review username for all the movies in the list.
+    '''
+    movie_sum = []
+    for movie in movie_name:
+        movie_info = {}
+        movie_info["movie_title"] = movie
+        movie_info["imdb_id"] = imdb.search_for_title(movie)[0]["imdb_id"]
+
+        reviews = imdb.get_title_user_reviews(movie_info["imdb_id"])
+        movie_info["number_of_reviews"] = len(reviews["reviews"])
+        
+        movie_info['review_info']=[]
+        for i in range(5):
+            # len(reviews["reviews"]) - 1
+            each_r = {'username':reviews["reviews"][i]['author']['displayName'],
+                    'imdb_rating':reviews['reviews'][i]['authorRating'],
+                    'review_text': reviews['reviews'][i]['reviewText']
+                    }
+            movie_info['review_info'].append(each_r)
+
+        movie_sum.append(movie_info)
+    return movie_sum
+    # pprint.pprint(movie_sum)
 
 
-# Pickling 
+full_review_text = get_info(movie_name)
+# print(imdb.search_for_title('Star Wars: Episode I - The Phantom Menace')[0])
+
+'''The format of the new dictionary
+{movie name: 'Star Wars: Episode IV - A New Hope',
+movie_id: 'tt0076759',
+number_of_reviews: __,
+review_text:
+    [{
+    username: ___,
+    review_text:___,
+    user_rating: ___
+    }]'''
+
+
+
+# Pickling & save data to a file called review data
 import pickle
 
-# save data to a file
-with open('example_file.pickle','wb') as f:
-    pickle.dump(review_text,f)
-
+with open('review_data.pickle','wb') as f:
+    pickle.dump(full_review_text,f)
