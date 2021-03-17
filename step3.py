d251e05c6e963b642bad8f135ea0c0442fc340bc
Jan Sirimongkolkasem <psirimongkolkasem1@babson.edu>
2021-03-17 19:25:59 +0700

    updatedd

---
+++ step3.py
@@ -0,0 +1,22 @@
+# Load data
+import nltk
+import pickle
+import pprint
+
+with open('review_data.pickle','rb') as data_input:
+    reloaded_review_text = pickle.load(data_input)
+
+
+# pprint.pprint(reloaded_review_text)
+# print(type(reloaded_review_text))
+
+# Sentiment Analysis
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sent_dict = {}
+for i in reloaded_review_text:
+    s = SentimentIntensityAnalyzer().polarity_scores(reloaded_review_text[i])
+    if i not in sent_dict:
+        sent_dict[i] = s
+
+print(sent_dict)
