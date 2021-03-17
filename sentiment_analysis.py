11c8469b1e0533e5e97a9fa0017958537c7c01d7
emmymmma <77707333+emmymmma@users.noreply.github.com>
2021-03-17 15:35:39 -0400

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -2,9 +2,13 @@ import nltk
 import pickle
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
+
+# unpickling the two texts
 with open('saved_texts.pickle','rb') as input_file:
     Wuthering_Height = pickle.load(input_file)
     Jane_Eyre = pickle.load(input_file)
+    
+# general the sentiment score for the two texts
 score1 = SentimentIntensityAnalyzer().polarity_scores(Wuthering_Height)
 score2 = SentimentIntensityAnalyzer().polarity_scores(Jane_Eyre)
 print("Wuthering Height: ", score1)
