698a2f7093b6481ebaf5e174b2f2d6cebd5b2ba4
arosemond1 <77635551+arosemond1@users.noreply.github.com>
2021-03-17 07:49:10 -0400

    edits

--- assignment2.py
+++ assignment2.py
@@ -11,25 +11,32 @@ imdb = Imdb()
 # print(reviews['reviews'][0]['author']['displayName'])
 # print(reviews['reviews'][0]['reviewText'])
 
+
 def get_imdb_reviews(movie_title):
     """
     Function that returns all the reviews for a given movie title
     """
     movie = imdb.search_for_title(movie_title)[0]
-    imdb_id = movie['imdb_id']
+    imdb_id = movie["imdb_id"]
     reviews = imdb.get_title_user_reviews(imdb_id)
     return reviews
 
+
 # print(get_imdb_reviews("Parasite"))
 
-def get_review_text(reviews, review_number):
+
+def get_review_text(reviews, review_number=0, rand=True):
     """
     Function that returns a random review from the first 20 reviews of a movie
     """
     import random
-    # random_number = random.randint(1, 20)
-    # review_text = reviews['reviews'][random_number]['reviewText']
-    review_text = reviews['reviews'][review_number]['reviewText']
+
+    random_number = random.randint(1, 20)
+
+    if rand == True:
+        review_text = reviews["reviews"][random_number]["reviewText"]
+    else:
+        review_text = reviews["reviews"][review_number]["reviewText"]
     return review_text
 
 
@@ -39,6 +46,7 @@ def histogram(s):
     s = a string or a list
     """
     import string
+
     d = dict()
     clean_review = []
     # print(clean_review)
@@ -57,40 +65,42 @@ def histogram(s):
     return d
 
 
-def sorted_hist(hist):
-    """Sorts a word frequency dictionary"""
-    lst=[]
-    for key, values in hist.items():
+def sorted_hist(hist, exclude_stopwords=True):
+    """Sorts a word frequency dictionary from greatest to smallest frequency
+    hist : dictionary with word frequency"""
+
+
+    lst = []
+    stopwords = open("stopword.txt", encoding="UTF8")
+    # print(stopwords)
+    hist_int = hist.copy()
+    lst_st_words = []
+    for line in stopwords:
+        lst_st_words.append(line.strip())
+    # print(lst_st_words)
+    if exclude_stopwords == True:
+        for word in hist:
+            if word in lst_st_words:
+                hist_int.pop(word)
+    # return hist_int
+    for key, values in hist_int.items():
         t = (values, key)
         lst.append(t)
     sort_lst = sorted(lst, reverse=True)
     return sort_lst
 
 
-from scipy import spatial
-
-# print(help(scipy))
-reviews = get_imdb_reviews("Parasite")
-corpus = []
-review1 = get_review_text(reviews, 0)
-review2 = get_review_text(reviews, 1)
-
-spatial.distance.cosine(review1, review2)
-
-
 def main():
     reviews = get_imdb_reviews("Parasite")
     # print(reviews)
-    # review_text = get_review_text(reviews, 0)
+    review_text = get_review_text(reviews, 0, rand=False)
     # print(review_text)
     # print(type(review_text))
-    # hist = (histogram(review_text))
+    hist = histogram(review_text)
     # print(hist)
-    # print(sorted_hist(hist))
+    import pprint
+    pprint.pprint(sorted_hist(hist))
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
